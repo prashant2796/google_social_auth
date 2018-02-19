2fa629cd36607638494ff0a4c26edfd9eda2d68c
prashant <prashantpandey071@gmail.com>
2018-02-19 23:08:24 +0530

    changed the scope to userinfo.profie

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ AUTHORIZE_URL='https://accounts.google.com/o/oauth2/auth?'                      
 CALLBACK_URL='http://127.0.0.1:5000/oauth2callback'								#this is the url of your website.The user is redirected to this url after the user has authorize your app.											 
 ACCESS_TOKEN_URL='https://www.googleapis.com/oauth2/v3/token'				#get or post request to this url gives back the access token
 API_RESOURCE_URL='https://www.googleapis.com/oauth2/v1/userinfo'          #get request to this url gives back the desired user info.
-SCOPE_URL='https://www.googleapis.com/auth/user.emails.read'    #since we need users profile information 
+SCOPE_URL='https://www.googleapis.com/auth/userinfo.profile'    #since we need users profile information 
